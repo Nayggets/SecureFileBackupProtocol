ba06eb873a051bddfdc49ff734b1160ba0f6820c
Nayggets <quentin.melotte@gmail.com>
2023-12-22 12:47:01 +0100

    Script d'installation terminé

--- InstallServer.sh
+++ InstallServer.sh
@@ -1,2 +1,4 @@
 make install_package 
 make build_server
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -sha256 -out /home/$USER/server.crt -keyout server.key  1>/dev/null
+cp ./build/client/client.elf /home/$USER/client.elf
--- installClient.sh
+++ installClient.sh
@@ -1,3 +1,4 @@
 make install_package 
 make build_client
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -sha256 -out /home/$USER/mycert.pem  1>/dev/null
 cp ./build/client/client.elf /home/$USER/client.elf
