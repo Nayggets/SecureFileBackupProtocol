462a2f97b0612a33e6285289c336d7eef3354480
Nayggets <quentin.melotte@gmail.com>
2024-01-11 23:48:42 +0100

    Commit last bug fix

--- installServer.sh
+++ installServer.sh
@@ -1,6 +1,6 @@
+mkdir ./build/server/
 make install_package 
 make build_server
-mkdir ./build/server/
 mkdir /home/$USER/server
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -sha256 -out /home/$USER/server/server.crt -keyout /home/$USER/server/server.key  1>/dev/null
