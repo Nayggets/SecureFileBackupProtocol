190fc31e083230c444bd43e283c8e1da3c285479
Nayggets <quentin.melotte@gmail.com>
2024-01-11 23:42:42 +0100

    Fix bug server build

--- installServer.sh
+++ installServer.sh
@@ -1,6 +1,7 @@
 make install_package 
 make build_server
+mkdir ./build/server/
 mkdir /home/$USER/server
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -sha256 -out /home/$USER/server/server.crt -keyout /home/$USER/server/server.key  1>/dev/null
-cp ./build/server/server.elf /home/$USER/server/server.elf
+sudo cp ./build/server/server.elf /home/$USER/server/server.elf
