30934790ea3c766ef155dce36d7a57402287294b
Nayggets <quentin.melotte@gmail.com>
2024-01-12 00:11:27 +0100

    Install Client fix bug

--- installClient.sh
+++ installClient.sh
@@ -1,6 +1,7 @@
 make install_package 
 make build_client
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -sha256 -out /home/$USER/mycert.pem  1>/dev/null
+rm privkey.pem
 cp ./build/client/client.elf /home/$USER/client.elf
 cp ./build/client/files.accept /home/$USER/files.accept
 cp ./build/client/autosave /home/$USER/autosave
